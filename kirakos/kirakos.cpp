ec4dad5005db37bd2dbd93cbe6d8b341f28a9eee
Bence2003Ecneb <101479583+BawskeeNBA@users.noreply.github.com>
2025-12-05 09:03:35 +0100

    formázva a tábla, csak a megadott gombokra reagál

--- kirakos/kirakos.cpp
+++ kirakos/kirakos.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <iomanip>
+#include <conio.h>
 using namespace std;
 
 const int x = 4, y = 4;
@@ -10,21 +11,72 @@ int tabla[x][y] = {
     { 8, 15, 13, 0 }
 };
 
+int uresSor = 3;
+int uresOszlop = 3;
+char irany;
+
+
+
 void kiir() {
+
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             if (tabla[i][j] == 0)
-                cout << " . ";
+                cout << setw(3) << "_";   // üres mezõ
             else
-                cout << tabla[i][j] << " ";
+                cout << setw(3) << tabla[i][j];
         }
         cout << endl;
     }
 }
 
+
+void mozgat(char irany) {
+    int ujSor = uresSor;
+    int ujOszlop = uresOszlop;
+
+    if (irany == 'w') ujSor--;      // fel
+    else if (irany == 's') ujSor++; // le
+    else if (irany == 'a') ujOszlop--; // bal
+    else if (irany == 'd') ujOszlop++; // jobb
+
+    // hatar ellenorzes
+    if (ujSor >= 0 && ujSor < 4 && ujOszlop >= 0 && ujOszlop < 4) {
+        int temp = tabla[uresSor][uresOszlop];
+        tabla[uresSor][uresOszlop] = tabla[ujSor][ujOszlop];
+        tabla[ujSor][ujOszlop] = temp;
+
+        // ures mezo frissitese
+        uresSor = ujSor;
+        uresOszlop = ujOszlop;
+    }
+}
+
+
 int main()
 {
-    kiir();
+    while (true)
+    {
+        system("cls");
+        kiir();
+
+        std::cout << "\n(billentyuk: w a s d, '-' = kilepes)\n";
+
+        char beker = _getch();//_getch olvassa a karaktert egybol
+        beker = tolower(beker);
+
+        if (beker == '-')
+            break;
+
+        if (beker == 'w' || beker == 'a' || beker == 's' || beker == 'd')
+        {
+            mozgat(beker);
+        }
+    }
+
+    std::cout << "\nKileptel a jatekbol.\n";
+    return 0;
 }
+
 
 
